01d5af1ec68e4bc3d1bb88fbbda024bc838ff006
Miri <Miri@MIRI-PC>
2026-02-23 12:28:10 +0200

    Initial commit

--- web.py
+++ web.py
@@ -8,19 +8,20 @@ st.title("Todos")
 st.subheader("This is my todo app")
 st.write("Tis app is to increase your productivity.")
 
-for index,todo in enumerate(todos):
-   checkbox= st.checkbox(todo,key=todo)
-   if checkbox:
-       todos.pop(index)
-       functions.write_todos(todos)
-       del st.session_state[todo]
-       st.rerun()
+for index, todo in enumerate(todos):
+    label = todo.strip()
+    if st.checkbox(label, key=f"todo_{index}"):
+        todos.pop(index)
+        functions.write_todos(todos)
+        st.rerun()
 
 def add_todo_new():
-    st.session_state["new_todo"]
-    todos.append(todo +"\n")
+    new_todo = st.session_state["new_todo"].strip()
+    if not new_todo:
+        return
+    todos.append(new_todo + "\n")
     functions.write_todos(todos)
-
+    st.session_state["new_todo"] = ""
 
 st.text_input(label="",placeholder="Enter todo",
                        on_change=add_todo_new,key='new_todo')
