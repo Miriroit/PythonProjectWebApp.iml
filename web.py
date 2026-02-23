698c42e266915fddfffa1e65a8c217011edc867a
Miri <Miri@MIRI-PC>
2026-02-23 13:14:11 +0200

    Initial commit 1

--- web.py
+++ web.py
@@ -1,20 +1,6 @@
 import streamlit as st
 import  functions
 
-
-todos = functions.get_todos()
-
-st.title("Todos")
-st.subheader("This is my todo app")
-st.write("Tis app is to increase your productivity.")
-
-for index, todo in enumerate(todos):
-    label = todo.strip()
-    if st.checkbox(label, key=f"todo_{index}"):
-        todos.pop(index)
-        functions.write_todos(todos)
-        st.rerun()
-
 def add_todo_new():
     new_todo = st.session_state["new_todo"].strip()
     if not new_todo:
@@ -23,8 +9,24 @@ def add_todo_new():
     functions.write_todos(todos)
     st.session_state["new_todo"] = ""
 
+todos = functions.get_todos()
+
+st.title("Todos")
+st.subheader("This is my todo app")
+st.write("Tis app is to increase your productivity.")
+
 st.text_input(label="",placeholder="Enter todo",
                        on_change=add_todo_new,key='new_todo')
+
+for index, todo in enumerate(todos):
+
+    if st.checkbox(todo,key=todo):
+        todos.pop(index)
+        functions.write_todos(todos)
+        st.rerun()
+
+
+
 
 #st.session_state
 #hfh
